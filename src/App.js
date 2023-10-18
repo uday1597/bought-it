e253c04c20922ac54fcd73efbeac2e344f91e859
uday1597 <udaysurya1597@gmail.com>
2023-10-18 23:38:58 +0530

    created frontend folder

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = () => {
-  return <h1>Welcome to BoughtIt!</h1>;
-};
-
-export default App;
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
