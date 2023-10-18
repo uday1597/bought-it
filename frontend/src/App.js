9e766e01218fa683dc4712225ae472ffae1b74f0
uday1597 <udaysurya1597@gmail.com>
2023-10-19 00:39:05 +0530

    roting and product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <h1>Welcome to BoughtIt!</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
