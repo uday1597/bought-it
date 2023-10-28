1b5b0bb6a666ff0978c4d8e1bc0b02b5c04d2854
uday1597 <udaysurya1597@gmail.com>
2023-10-28 16:13:50 +0530

    changes till Get products from database, Error middleware and Product controller

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import products from "./data/products.js";
-
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import router from "./routes/productRoutes.js";
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -11,13 +14,10 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", router);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
