233dad8338070bbe028bf9feb01645f22dcff7d8
“uday1597” <udaysurya1597@gmail.com>
2025-07-20 19:29:28 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRouter from "./routes/productRoutes.js";
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import cors from 'cors';
 
 const port = process.env.PORT || 5000;
 
 connectDB();
+dotenv.config();
 
 const app = express();
-const cors = require('cors');
 
-const allowedOrigins = ['http://localhost:3000', 'https://bought-it.vercel.app'];
+const allowedOrigins = [
+  'https://bought-it.vercel.app',
+  'http://localhost:3000' 
+];
 
 app.use(cors({
   origin: allowedOrigins,
-  credentials: true,
+  credentials: true 
 }));
 
 
